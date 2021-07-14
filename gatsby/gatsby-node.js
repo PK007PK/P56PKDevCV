25eaa2313ecc3cfd3ade5a4450fd843ad61055ef
Piotr Krasny <krasny.piotr@gmail.com>
2021-07-14 11:32:59 +0200

    Fix async / await error in createCategories

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -5,11 +5,8 @@ import { turnTagsIntoPages } from './src/gatsby/turnTagsIntoPages';
 export { createSchemaCustomization } from './src/gatsby/createSchemaCustomization';
 export { onCreateNode } from './src/gatsby/onCreateNode';
 
+// Create pages dynamically
+// Wait for all promises to be resolved before finishing this function
 export async function createPages(params) {
-    // await Promise.all([createBlogPages(params)]);
-    // await Promise.all([turnCategoriesIntoPages(params)]);
-    // await Promise.all([turnTagsIntoPages(params)]);
-    await Promise.all([createBlogPages(params), turnCategoriesIntoPages(params), turnTagsIntoPages(params)]);
-    // await Promise.all([turnCategoriesIntoPages(params)]);
-    // await Promise.all([turnTagsIntoPages(params)]);
+    await Promise.all([turnCategoriesIntoPages(params)]);
 }
