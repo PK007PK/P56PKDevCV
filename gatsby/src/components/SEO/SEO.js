a4c6293cbff4e5acba6f43888d23247940c9c7b8
Piotr Krasny <krasny.piotr@gmail.com>
2021-08-05 16:47:17 +0200

    SEO component is fine

--- gatsby/src/components/SEO/SEO.js
+++ gatsby/src/components/SEO/SEO.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 
-const SEO = ({ children, location, title, image }) => {
+const SEO = ({ children, location, title }) => {
     const data = useStaticQuery(graphql`
         {
             sanitySiteSettings {
